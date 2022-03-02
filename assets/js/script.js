531de062a383ed93d30e2f42a4e445422c8806d3
vanderpatrick <patrick123321123@icloud.com>
2022-03-02 17:10:10 +0000

    Add styles to navbar links and onClick function to redirect html pages

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// functions to call other pages
+function home() {
+    window.location.href = "index.html"
+}
+function aboutUs() {
+    window.location.href = "about_us.html"
+}
+function myStory() {
+    window.location.href = "my_story.html"
+}
+function poems() {
+    window.location.href = "poems.html"
+}
+
+
